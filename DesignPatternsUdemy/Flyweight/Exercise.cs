569bb0429a0cd9c13db9eb974833a375b49bed61
Nabid Imteaj <nabid.imteaj@live.com>
2021-09-06 00:01:31 +0200

    adding Flyweight exercise solution

---
+++ DesignPatternsUdemy/Flyweight/Exercise.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatternsUdemy.Flyweight
+{
+    public static class Exercise
+    {
+        public static void Demo()
+        {
+            var sentence = new Sentence("hello new world in visual studio");
+            sentence[1].Capitalize = true;
+            sentence[4].Capitalize = true;
+            Console.WriteLine(sentence);
+        }
+    }
+
+    public class Sentence
+    {
+        private string[] words;
+        private Dictionary<int, WordToken> tokens = new Dictionary<int, WordToken>();
+
+        public Sentence(string plainText)
+        {
+            words = plainText.Split(' ');
+        }
+
+        public WordToken this[int index]
+        {
+            get
+            {
+                WordToken wt = new WordToken();
+                tokens.Add(index, wt);
+                return tokens[index];
+            }
+        }
+
+        public override string ToString()
+        {
+            var finalWords = new List<string>();
+            for (var i = 0; i < words.Length; i++)
+            {
+                var word = words[i];
+                if (tokens.ContainsKey(i) && tokens[i].Capitalize)
+                {
+                    word = word.ToUpperInvariant();
+                }
+                finalWords.Add(word);
+            }
+            return string.Join(" ", finalWords);
+        }
+
+        public class WordToken
+        {
+            public bool Capitalize;
+        }
+    }
+}
--- DesignPatternsUdemy/Program.cs
+++ DesignPatternsUdemy/Program.cs
@@ -62,6 +62,10 @@ namespace DesignPatternsUdemy
             #region Facade
 
             #endregion
+
+            #region Flyweight
+            Flyweight.Exercise.Demo();
+            #endregion
         }
     }
 }
