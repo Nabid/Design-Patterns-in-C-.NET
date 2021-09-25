00194764f92508c69c03e92b5205aa30dd829ff1
Nabid Imteaj <nabid.imteaj@live.com>
2021-09-26 00:08:16 +0200

    Chain of responsibility exercise solution

--- DesignPatternsUdemy/ChainOfResponsibility/Exercise.cs
+++ DesignPatternsUdemy/ChainOfResponsibility/Exercise.cs
@@ -22,52 +22,111 @@ namespace DesignPatternsUdemy.ChainOfResponsibility
 
     public abstract class Creature
     {
-        public int Attack { get; set; }
-        public int Defense { get; set; }
+        protected Game game;
+        protected readonly int baseAttack;
+        protected readonly int baseDefense;
 
+        protected Creature(Game game, int baseAttack, int baseDefense)
+        {
+            this.game = game;
+            this.baseAttack = baseAttack;
+            this.baseDefense = baseDefense;
+        }
+
+        public virtual int Attack { get; set; }
+        public virtual int Defense { get; set; }
+        public abstract void Query(object source, StatQuery sq);
     }
 
     public class Goblin : Creature
     {
-        public Goblin(Game game)
+        public override void Query(object source, StatQuery sq)
         {
-            Attack = 1;
-            Defense = 1;
-
-            foreach (var creature in game.Creatures)
+            if (ReferenceEquals(source, this))
             {
-                if (creature is Goblin)
+                switch (sq.Statistic)
                 {
-                    creature.Defense += 1;
+                    case Statistic.Attack:
+                        sq.Result += baseAttack;
+                        break;
+                    case Statistic.Defense:
+                        sq.Result += baseDefense;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
                 }
             }
+            else
+            {
+                if (sq.Statistic == Statistic.Defense)
+                {
+                    sq.Result++;
+                }
+            }
+        }
+
+        public override int Defense
+        {
+            get
+            {
+                var q = new StatQuery { Statistic = Statistic.Defense };
+                foreach (var c in game.Creatures)
+                    c.Query(this, q);
+                return q.Result;
+            }
+        }
+
+        public override int Attack
+        {
+            get
+            {
+                var q = new StatQuery { Statistic = Statistic.Attack };
+                foreach (var c in game.Creatures)
+                    c.Query(this, q);
+                return q.Result;
+            }
+        }
+
+        public Goblin(Game game) : base(game, 1, 1)
+        {
+        }
+
+        protected Goblin(Game game, int baseAttack, int baseDefense) : base(game,
+          baseAttack, baseDefense)
+        {
         }
     }
 
     public class GoblinKing : Goblin
     {
-        public GoblinKing(Game game) : base(game)
+        public GoblinKing(Game game) : base(game, 3, 3)
         {
-            Attack = 3;
-            Defense = 3;
+        }
 
-            foreach (var creature in game.Creatures)
+        public override void Query(object source, StatQuery sq)
+        {
+            if (!ReferenceEquals(source, this) && sq.Statistic == Statistic.Attack)
             {
-                if(creature is Goblin)
-                {
-                    creature.Attack += 1;
-                }
+                sq.Result++; // every goblin gets +1 attack
             }
+            else base.Query(source, sq);
         }
     }
 
     public class Game
     {
-        public IList<Creature> Creatures;
+        public IList<Creature> Creatures = new List<Creature>();
+    }
 
-        public Game()
-        {
-            Creatures = new List<Creature>();
-        }
+    public enum Statistic
+    {
+        Attack,
+        Defense
+    }
+
+    public class StatQuery
+    {
+        public Statistic Statistic;
+        public int Result;
     }
 }
